3feb21488c32faf690446ff8905f142b740684f8
Charisma Khawaja <actuallycharisma@outlook.com>
2024-05-13 13:51:43 +0100

    first paired commit

---
+++ src/main/java/Artist.java
@@ -0,0 +1,15 @@
+public class Artist {
+
+    private String name;
+
+    //constructor
+    public Artist(String name){
+        this.name = name;
+    }
+
+    public String getName(){
+        return name;
+    }
+    
+
+}
---
+++ src/test/java/ArtistTest.java
@@ -0,0 +1,23 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+public class ArtistTest {
+
+    Artist artist;
+
+    @BeforeEach
+    public void setUp(){
+        artist = new Artist("Gaudi");
+    }
+
+    @Test
+    public void artistHasName(){
+        assertThat(artist.getName()).isEqualTo("Gaudi");
+    }
+
+
+
+
+}
